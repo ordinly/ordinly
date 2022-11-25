eb971d3b32e293eca98743335330debee25619bc
SeanAllanMacKay <seanallanmackay@gmail.com>
2022-11-25 09:05:21 -0600

    remove webpackDevMiddleware from next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -10,13 +10,5 @@ module.exports = {
     return config;
   },
 
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-
   reactStrictMode: false,
 };
